9590d8e8eb5bb0ae4767f20342c3c1525da03d41
Imke Redecker <imke.redecker@gmail.com>
2024-06-27 20:52:35 +0200

    update fanny page

--- pages/Fanny.vue
+++ pages/Fanny.vue
@@ -1,3 +1,17 @@
 <template>
     Fanny
-</template>
+    Hier kommt die beschreibung des Projekts
+    <UButton>Hello</UButton>
+    <UCard>
+      <template #header>
+        <Placeholder class="h-8" />
+      </template>
+  
+      <Placeholder class="h-32" />
+  
+      <template #footer>
+        <Placeholder class="h-8" />
+      </template>
+    </UCard>
+  </template>
+

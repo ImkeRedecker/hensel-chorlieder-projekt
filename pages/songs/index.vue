01f6e7932da270ffc58b79e2cfac7a570d482eb6
Imke Redecker <imke.redecker@gmail.com>
2024-07-14 17:22:37 +0200

    hensel-Komm reparierte Fassung

--- pages/songs/index.vue
+++ pages/songs/index.vue
@@ -6,5 +6,6 @@
     <UButton to="/songs/hensel-im-herbste">Im Herbste</UButton>
     <UButton to="/songs/hensel-lockung">Lockung</UButton>
     <UButton to="/songs/hensel-im-wald">Im Wald</UButton>
+    <UButton to="/songs/hensel-abendlich-schon">Abendlich Schon</UButton>
 
 </template>

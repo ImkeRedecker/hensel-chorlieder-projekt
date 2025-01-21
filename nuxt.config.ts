e4f1316025b9d88448b513078b958b9d49fbe43e
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-01-22 00:48:12 +0100

    Improve indentation

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,28 +1,28 @@
-import {fileURLToPath} from 'node:url';
+import { fileURLToPath } from 'node:url';
 
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-  compatibilityDate: '2025-01-22',
-  devtools: { enabled: false },
-  modules: ["@nuxt/ui"],
-  vite: {
-    worker: {
-        format: 'es',
+    compatibilityDate: '2025-01-22',
+    devtools: { enabled: false },
+    modules: ["@nuxt/ui"],
+    vite: {
+        worker: {
+            format: 'es',
+        },
+        optimizeDeps: {
+            exclude: ['verovio'],
+        },
     },
-    optimizeDeps: {
-        exclude: ['verovio'],
+    nitro: {
+        publicAssets: [
+            {
+                baseURL: 'kern/hensel-chorlieder',
+                dir: fileURLToPath(new URL('./hensel-chorlieder/kern', import.meta.url)),
+                maxAge: 3600,
+            },
+        ],
     },
-  },
-  nitro: {
-    publicAssets: [
-      {
-        baseURL: 'kern/hensel-chorlieder',
-        dir: fileURLToPath (new URL('./hensel-chorlieder/kern', import.meta.url)),
-        maxAge: 3600,
-      },
-    ],
-  },
-  colorMode: {
-    preference: 'light',
-  },
+    colorMode: {
+        preference: 'light',
+    },
 });

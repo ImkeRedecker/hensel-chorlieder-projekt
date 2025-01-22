5872eb7ebf00a864431135b521ccdeb75b5b777f
Wolfgang Drescher <drescher.wolfgang+git@gmail.com>
2025-01-22 21:42:49 +0100

    Add pages for Chorlieder

---
+++ composables/piece-filter.js
@@ -0,0 +1,20 @@
+import { defineStore } from 'pinia';
+
+const defaultFilter = {
+    hideLyrics: false,
+    hideDynamics: false,
+    pianoReduction: false,
+    autobeam: false,
+    bassstufen: false,
+    fb: false,
+    outerVoices: false,
+};
+
+export const usePieceFilterStore = defineStore('piece_filter', {
+    state: () => ({...defaultFilter}),
+    actions: {
+        reset() {
+            this.$patch({...defaultFilter});
+        },
+    },
+});
